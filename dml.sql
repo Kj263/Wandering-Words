75d064ae7efbba617f010b354d928d4697ef4c6f
birdybird10 <allimajors22@gmail.com>
2024-05-05 17:43:01 -0500

    cleaned up, but not in sync with html yet

--- dml.sql
+++ dml.sql
@@ -8,69 +8,51 @@ FROM Members
 -- add a new member for the Members Page
 INSERT INTO Members (memberFirstName, memberLastName, memberEmail) 
 VALUES (:memberFirstNameInput, :memberLastNameInput, :memberEmailInput)
+
+
+
+
 --------------------------------------------------------------------------
-
-
-
-
 -- BOOKS
 -- display all books
 SELECT bookID, bookTitle, genre, numCopies 
 FROM Books
 
--- display all books with their author (needed or no?)
-SELECT Books.bookID, bookTitle, genre, numCopies, authorLastName AS author
-FROM Books
-INNER JOIN BooksAuthors ON Books.bookID = BooksAuthors.bookID
-INNER JOIN Authors on Authors.authorID = BooksAuthors.authorID
-
 -- add a new book 
 INSERT INTO Books (bookTitle, genre, numCopies) 
 VALUES (:bookTitleInput, :genreInput, :numCopiesInput)
 
--- give a book an author/authors (M:M relationship) (needed or no?)
-INSERT INTO BooksAuthors (bookID, authorID)
-VALUES (:bookIDInput, :authorIDInput)
+
 
 --------------------------------------------------------------------------
-
-
-
 -- AUTHORS
 -- display all authors
 SELECT authorID, authorFirstName, authorLastName 
 FROM Authors
 
--- display all authors with their respective books
-SELECT Authors.authorID, authorFirstName, authorLastName, bookTitle
-FROM Authors
-INNER JOIN BooksAuthors ON Authors.authorID = BooksAuthors.authorID
-INNER JOIN Books on Books.bookID = BooksAuthors.bookID
-
 -- add a new author
 INSERT INTO Authors (authorFirstName, authorLastName)
 VALUES (:authorFirstNameInput, :authorLastNameInput)
 
+
+
 --------------------------------------------------------------------------
+-- CHECKOUTS PAGE/CHECKED-OUT BOOKS 
 
-
-
--- CHECKOUTS PAGE/Checked-out books page?
 -- get all memberID's and memberLastName's for Member dropdown
 SELECT memberID, memberLastName FROM Members
 
 -- get all employeeID's and employeeLastName's for Employee dropdown
 SELECT employeeID, employeeLastName FROM Employees
 
--- display all checkouts 
-SELECT checkoutID, dateCheckedOut, dateDue, 
-Members.memberLastName AS memberLastName, Employees.employeeLastName AS employeeLastName
+-- display all checkouts
+SELECT checkoutID, Members.memberLastName AS memberLastName, Employees.employeeLastName AS employeeLastName,
+dateCheckedOut, dateDue
 FROM Checkouts
 INNER JOIN Members ON Checkouts.memberID = Members.memberID
 INNER JOIN Employees ON Checkouts.employeeID = Employees.employeeID
 
-
--- display all checkouts with their respective books and the date returned
+-- display all checkouts with their respective books and dateReturned
 SELECT Checkouts.checkoutID, bookTitle, dateReturned
 FROM Checkouts
 INNER JOIN BooksCheckouts ON Checkouts.checkoutID = BooksCheckouts.checkoutID
@@ -79,11 +61,11 @@ ORDER BY checkoutID
 
 -- add a new checkout
 INSERT INTO Checkouts (memberID, employeeID, dateCheckedOut, dateDue) 
-VALUES (:memberID_from_dropdown_Input, :employeeID_from_dropdown_input, :dateCheckedOutInput, :dateDueInput)
+VALUES (:memberID_from_dropdown_input, :employeeID_from_dropdown_input, :dateCheckedOutInput, :dateDueInput)
 
 -- add books to a checkout (adding to a M:M relationship)
-INSERT INTO BooksCheckouts (bookID, checkoutID, dateReturned)
-VALUES (:bookIDInput, :checkoutIDInput, :dateReturnedInput)
+INSERT INTO BooksCheckouts (bookID, checkoutID)
+VALUES (:bookID_from_dropdown_input, :checkoutID_from_dropdown_input)
 
 -- update the dateReturned in BooksCheckouts (updating a M:M relationship)
 UPDATE BooksCheckouts
@@ -93,19 +75,19 @@ WHERE BooksCheckouts.bookID = :bookID_from_update_form
 -- delete a checkout
 DELETE FROM Checkouts WHERE checkoutID = :checkoutIDInput
 
--- dis-associate books from a checkout (deleting a M:M relationship) (is this needed or does cascade take care of this??)
+-- dis-associate a checkout from its books (deleting a M:M relationship) 
 DELETE FROM BooksCheckouts WHERE checkoutID = :checkoutIDInput AND bookID = :bookIDInput
 
 -- update a checkout
 UPDATE Checkouts 
-SET employeeID = :employeeID_from_dropdown_input, dateDue = :dateDueInput
+SET memberID = :memberID_from_dropdown_input, employeeID = :employeeID_from_dropdown_input, 
+dateCheckedOut = :dateCheckedOutInput, dateDue = :dateDueInput
 WHERE checkoutID = :checkoutID_from_update_form
 
+
+
+
 --------------------------------------------------------------------------
-
-
-
-
 -- EMPLOYEES PAGE
 -- display all employees
 SELECT employeeFirstName, employeeLastName, employeeEmail 
@@ -114,4 +96,3 @@ FROM Employees
 -- add a new employee
 INSERT INTO Employees (employeeFirstName, employeeLastName, employeeEmail) 
 VALUES (:employeeFirstNameInput, :employeeLastNameInput, :employeeEmailInput)
-
