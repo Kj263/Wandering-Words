709c64800fc33c76ab7f13d039545a83634e594b
kfast <114452573+Kj263@users.noreply.github.com>
2024-05-20 18:22:38 -0500

    Stylesheet Added, Window reload added to delete and update checkout.  Main template updated to reference stylesheet and add navbar

--- public/js/delete_checkout.js
+++ public/js/delete_checkout.js
@@ -1,26 +1,27 @@
 function deleteCheckout(checkoutID) {
-    let link = '/delete-checkout-ajax/';
-    let data = {
-      id: checkoutID
-    };
-  
-    $.ajax({
-      url: link,
-      type: 'DELETE',
-      data: JSON.stringify(data),
-      contentType: "application/json; charset=utf-8",
-      success: function(result) {
-        deleteRow(checkoutID);
-      }
-    });
-  }
-  
-  function deleteRow(checkoutID){
-      let table = document.getElementById("checkouts-table");
-      for (let i = 0, row; row = table.rows[i]; i++) {
-         if (table.rows[i].getAttribute("data-value") == checkoutID) {
-              table.deleteRow(i);
-              break;
-         }
-      }
-  }
+  let link = '/delete-checkout-ajax/';
+  let data = {
+    id: checkoutID
+  };
+
+  $.ajax({
+    url: link,
+    type: 'DELETE',
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    success: function(result) {
+      deleteRow(checkoutID);
+      window.location.reload()
+    }
+  });
+}
+
+function deleteRow(checkoutID){
+    let table = document.getElementById("checkouts-table");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+       if (table.rows[i].getAttribute("data-value") == checkoutID) {
+            table.deleteRow(i);
+            break;
+       }
+    }
+}
