1b79c98362ec200da678e8b95f5fb751f0d7137c
kfast <114452573+Kj263@users.noreply.github.com>
2024-05-21 11:24:13 -0500

    Adding File to Delete Book from Checkout in JS

---
+++ public/js/delete_book_in_checkout.js
@@ -0,0 +1,28 @@
+function deleteBookinCheckout(booksCheckoutsID) {
+    let link = '/delete-book-in-checkout-ajax/';
+    let data = {
+      id: booksCheckoutsID
+    };
+  
+    $.ajax({
+      url: link,
+      type: 'DELETE',
+      data: JSON.stringify(data),
+      contentType: "application/json; charset=utf-8",
+      success: function(result) {
+        deleteRow(booksCheckoutsID);
+        window.location.reload()
+      }
+    });
+  }
+  
+  function deleteRow(booksCheckoutsID){
+      let table = document.getElementById("books-checkouts-table");
+      for (let i = 0, row; row = table.rows[i]; i++) {
+         if (table.rows[i].getAttribute("data-value") == booksCheckoutsID) {
+              table.deleteRow(i);
+              break;
+         }
+      }
+  }
+  
